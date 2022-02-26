0a0fc16d1209c6b24a84ed4b6fead62981542b94
terellison <ellisonterry4@gmail.com>
2022-02-26 13:37:43 -0500

    I think I nailed it :)

--- MaxisTakeHome/MaxisTakeHome.cpp
+++ MaxisTakeHome/MaxisTakeHome.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include "Year.h"
+#include <map>
 #include "Composer.h"
 #include "IOHelper.h"
+#include "TimePeriod.h"
 
 int main()
 {
 	std::vector<Composer>* composerList = IOHelper::GetComposerData("input/dataset.txt");
-	std::vector<Year> timeline;
+	std::map<int, int> years;
 
 	for (auto& composer : *composerList)
 	{
-		std::cout <<
-			"Composer name: " << composer.name << std::endl <<
-			"Birth year: " << composer.birthYear << std::endl <<
-			"Death year: " << composer.deathYear << "\n\n";
+		int i = composer.birthYear;
+		while (i <= composer.deathYear)
+		{
+			++years[i];
+			++i;
+		}
 	}
+	int maxAlive = 0, mostRecentYear = 0;
+	TimePeriod* greatestTimePeriod = new TimePeriod();
+	for (auto &year : years)
+	{
+		if (year.first > greatestTimePeriod->endYear && year.second > greatestTimePeriod->composersAlive) // we've found a new time period to track
+		{
+			greatestTimePeriod->startYear = year.first;
+			greatestTimePeriod->composersAlive = year.second;
+			greatestTimePeriod->endYear = year.first;
+		}
+
+		else if (year.first > greatestTimePeriod->startYear && year.second == greatestTimePeriod->composersAlive)
+		{
+			++greatestTimePeriod->endYear;
+		}
+	}
+
+	std::cout <<
+		"\nThe most recent time period with the most composers alive is " <<
+		greatestTimePeriod->startYear << " - " << greatestTimePeriod->endYear << " with " << greatestTimePeriod->composersAlive << " composers alive." << std::endl;
 }
---
+++ MaxisTakeHome/TimePeriod.h
@@ -0,0 +1,9 @@
+#ifndef TIMEPERIOD_H
+#define TIMEPERIOD_H
+struct TimePeriod
+{
+	int startYear;
+	int endYear;
+	int composersAlive;
+};
+#endif
